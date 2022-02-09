4a4f86205d6ac2eab24b9af473daefed5169e74c
Irvingouj <49964599+Irvingouj@users.noreply.github.com>
2022-02-08 22:08:05 -0500

    delete unneceesary

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TopNavBar from './components/TopNavBar';
 function App() {
   return (
     <div className="App">
+      <TopNavBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> see what this looks like</h1>
       </header>
     </div>
   );
---
+++ src/components/TopNavBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TopNavBar() {
+  return (
+    <div>TopNavBar</div>
+  )
+}
+
+export default TopNavBar
